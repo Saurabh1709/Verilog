2064ce8906311e710d0191d77df6feb17cf05b93
Saurabh <40017924+Saurabh1709@users.noreply.github.com>
2023-07-16 00:16:04 +0530

    Update Operators and their applications.v

--- Operators and their applications.v
+++ Operators and their applications.v
@@ -12,6 +12,14 @@ module complement(b, a);
   assign b = ~a + 1;
 endmodule
 
+//multiplying a single bit to a each bit of a 4 bit number(a step for a 4 bit multiplier)
+module andasmul(a, b, c);
+  input [3:0]a;
+  input b;
+  output [3:0]c;
+  assign c = a & {4{b}};
+endmodule
+
 //Square
 module square (b, a);
   input [3:0]a;
@@ -33,6 +41,17 @@ module logics(c, a, b);
   output reg signed [3:0]c;
   assign c = a *2;
  
+endmodule
+
+//Best type of delay  where signal transition at input is not ignored
+module andasmul(a, b, c);
+  input a;
+  input b;
+  output reg c;
+  always@(*)
+    begin
+    c <= #4 a & b;  //Best type  of delay
+    end
 endmodule
 
 
