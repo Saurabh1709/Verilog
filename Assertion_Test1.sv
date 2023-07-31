1285baae63ae48d9d2a6ff0be72d2ae602402054
Saurabh <40017924+Saurabh1709@users.noreply.github.com>
2023-07-31 12:41:58 +0530

    Update Assertion_Test1.sv

--- Assertion_Test1.sv
+++ Assertion_Test1.sv
@@ -7,6 +7,24 @@ module tb;
       assert (a < b) 
         $display("GTG");
       else 
-        $error("Error Detected master Saurabh");
+        $error("Error Detected");
     end
 endmodule
+
+
+//Ex_2
+module tb;
+  logic a, b, c;
+  initial
+    begin
+      a = 1'b0;
+      b = 1'b0;
+      assert (a < b) 
+      else 
+        $error("Error Detected");    //Can use $fatal, $warning, $info(no problem)
+    end
+endmodule
+
+
+
+
