8ddb112558248c73c723b8bf50bbbe004e3dc6f8
Saurabh <40017924+Saurabh1709@users.noreply.github.com>
2023-08-24 17:56:08 +0530

    Update Modeling_Basic_Generator&driver.sv

--- Modeling_Communication_B/Modeling_Basic_Generator&driver.sv
+++ Modeling_Communication_B/Modeling_Basic_Generator&driver.sv
@@ -2,27 +2,28 @@ module tb;
   int a;
   int b;
   event done;
-//////////////////////////////////////////////Generator
-  initial begin  
+  event next;
+  
+  initial begin   ////////////////////////////Generator
     for(int i=0;i<10;i++) begin
       a=$urandom();
       $display("Sent %0d", a);
       #10;
+      wait(next.triggered);
     end
     -> done;
   end
-////////////////////////////////////////////////Driver
-  initial begin   
+  
+  initial begin   /////////////////////////////Driver
     forever begin
       #10;
       b=a;
       $display("Received %0d", b);
+      ->next;
     end
   end
   
-////////////////////////////////////////////Controller
-  
-  initial begin   
+  initial begin   ////////////////////////////Controller
     wait(done.triggered);
     $finish();
   end
