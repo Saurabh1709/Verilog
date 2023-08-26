efc8d0f0f8b1fb9b8d49db67cea3c50512e72a62
Saurabh <40017924+Saurabh1709@users.noreply.github.com>
2023-08-26 22:07:15 +0530

    Update Mailbox.sv

--- Modeling_Communication_B/Mailbox.sv
+++ Modeling_Communication_B/Mailbox.sv
@@ -1,3 +1,71 @@
+class transaction;
+  randc int a;
+  randc int b;
+  constraint aa {
+    a<40;
+    a>0;
+  }
+  constraint bb {
+    b<60;
+    b>40;
+  }
+endclass
+
+class generator;
+  mailbox mbx;
+  transaction t;
+  function new(mailbox  mbx);
+    this.mbx=mbx;
+  endfunction
+  
+  task run();
+  for(int i=0;i<10;i++) begin
+    t=new();
+    t.randomize();
+    $display("[SENT]: a= %0d, b= %0d", t.a, t.b);
+    mbx.put(t);
+    #10;
+  end
+  endtask
+endclass
+
+class driver;
+  mailbox mbx;
+  transaction t;
+  
+  function new(mailbox mbx);
+    this.mbx=mbx;
+  endfunction
+  
+  task run();
+    forever begin
+      t=new();
+      mbx.get(t);
+      $display("[RXD]: a= %0d, b= %0d", t.a, t.b);
+      #10;
+    end
+  endtask
+endclass
+
+module top;
+  driver drv;
+  generator gnt;
+  transaction t;
+  mailbox mbx;
+  initial begin
+    mailbox mbx;
+    mbx=new();
+    gnt=new(mbx);
+    drv=new(mbx);
+    fork
+      gnt.run();
+      drv.run();
+    join
+  end
+endmodule
+
+
+/*
 class generator;
   int data=11;
   mailbox mbx;
@@ -35,5 +103,6 @@ module tb;
     drv.run;
   end
 endmodule
+*/
     
     
