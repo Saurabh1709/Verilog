d2715b714496cbb6c96df94f9fd09f33fac9893a
Saurabh <40017924+Saurabh1709@users.noreply.github.com>
2023-09-06 12:38:19 +0530

    Update Interface.sv

--- Modeling_Communication_B/Interface.sv
+++ Modeling_Communication_B/Interface.sv
@@ -1,22 +1,42 @@
-interface inf_i;
+module add(input [3:0]a, b, output reg [4:0]c, input clk);
+  always@(posedge clk) begin
+    c=a+b;
+  end
+endmodule
+
+
+/////TB
+
+
+interface inf;
   logic [3:0]a, b;
-  logic [4:0]sum;
+  logic [4:0]c;
+  logic clk;
 endinterface
 
+class driver;
+  virtual inf infd;
+  task run();
+    repeat(2) @(posedge infd.clk);
+    infd.a<=4;
+    infd.b<=11;
+  endtask
+endclass
+
 module top;
-  inf_i inf();
-  sum sum_i(.x(inf.a), .z(inf.sum), .y(inf.b));
+  inf inft();
+  add dut(inft.a, inft.b, inft.c, inft.clk);
+  initial inft.clk<=0; 
+  driver drv;
   initial begin
-    inf.a=10;
-    inf.b=7;
-    #10;
-    inf.a=3;
-    #10;
-    inf.b=9;
+    drv=new();
+    drv.infd=inft;
+    drv.run;
   end
+  always #10 inft.clk=~inft.clk;
   initial begin
-    $dumpfile("dump.vcd");
     $dumpvars(1);
+    $dumpfile("dump.vcd");
     #100 $finish;
   end
 endmodule
